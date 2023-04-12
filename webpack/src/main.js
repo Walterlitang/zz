e2ee72878870943fda8a46d3c4bf832655fcf1fd
qflyqfl <1522906704@qq.com>
2023-04-12 19:03:22 +0800

    webpack修改

--- webpack/src/main.js
+++ webpack/src/main.js
@@ -1,7 +1,7 @@
 import count from "./js/count";
 import sum from "./js/sum";
 import "./css/count.css";
-import "./less/index.less"
+import "../less/count.less"
 console.log("count",count);
 console.log("sum",sum);
 console.log(count(10,20));
